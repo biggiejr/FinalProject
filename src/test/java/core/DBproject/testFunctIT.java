5ee3e2ce5c45dc5e0becb77c2eedb3ca036e7aad
teutzue_1995@yahoo.com <teutzue_1995@yahoo.com>
2017-05-10 13:56:57 +0200

    commit test

--- src/test/java/core/DBproject/testFunctIT.java
+++ src/test/java/core/DBproject/testFunctIT.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by CosticaTeodor on 08/05/2017.
+ * Created by Costica 08/05/2017.
  */
 public class testFunctIT {
 
