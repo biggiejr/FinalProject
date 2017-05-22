47e9fda02f561f876c1f4d1d7218f4b41b3845c8
biggiejr <jrmacej@gmail.com>
2017-05-22 20:11:08 +0200

    added neo second query

--- src/main/java/mappers/Neo4jMapper.java
+++ src/main/java/mappers/Neo4jMapper.java
@@ -51,8 +51,33 @@ public class Neo4jMapper implements DbMapper {
 
     public ArrayList<City> getMentionedCitiesByBook(String bookTitle) 
     {
-        return null;
-    }
+        Session s = connector.getSession();
+        ArrayList<City> cities = new ArrayList<City>();
+
+        String querry="MATCH (book:Book) - [:MENTIONED]->(city:City) "+
+                "WHERE book.title = {bookTitle} "+
+                "RETURN city.id, city.city, city.latitude, city.longitude";
+
+        StatementResult result=s.run(querry,Values.parameters( "bookTitle", bookTitle ) );
+
+
+        while(result.hasNext())
+        {
+            Record record=result.next();
+            Integer id=Wrapper.getInt(record.
+                    get("city.id"));
+
+            String name=record.get("city.name").asString();
+            Double latitude=record.get("city.latitude").asDouble();
+            Double longitude=record.get("city.longitude").asDouble();
+
+            cities.add(new City(name,latitude,longitude));
+
+        }
+
+
+        s.close();
+        return cities;    }
 
     public ArrayList<Book> getMentionedCitiesByAuthor(String author) 
     {
